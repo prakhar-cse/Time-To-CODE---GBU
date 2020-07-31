2693c968231f915c262fc27d27ff16ce29deed53
Prakhar Rastogi <67405869+prakhar-cse@users.noreply.github.com>
2020-07-31 17:16:49 +0530

    Create BRTNUM.cpp

---
+++ BRTNUM.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{  
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	
+	int  t; 
+	cin >> t; 
+	while(t--){
+		string a = "1729";
+		
+		string s;
+		cin>>s;
+			
+		if (s.find(a) != std::string::npos) {
+    std::cout << "Yes\n";  }
+	else
+  cout<<"No\n";
+		
+		
+		
+		
+	}
+	return 0; 
+} 
